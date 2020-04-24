59d7617d4e6d25283c19dd771f5581181705ac40
edwardsifi <edwardsifi0@gmail.com>
2020-04-23 18:31:29 -0600

    login echo en asp.net con sql server
    llogin echo en asp.net con sql server

---
+++ login3/login3/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using login3.Models;
+using System.Data.SqlClient;
+
+//para crear este controlador primero se agrego un controlador
+namespace login3.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //crear un metodo pra la cadena de coneccion
+        void connectionString() {
+
+            con.ConnectionString = "data source=ACERE11\\SQLEXPRESS; database=WPF; integrated security = SSPI;";
+        
+        }
+
+        //verificar nombre y contraseña
+        [HttpPost]
+        public ActionResult Verify(Account acc) {
+
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from login where username = '"+acc.Name+"' and password ='"+acc.Password+"' ";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+
+            }
+            
+
+            }
+    }
+    
+}
---
+++ login3/login3/Models/account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//para hacer este modelo primero se creo una clase vacia
+namespace login3.Models
+{
+    
+    //este es el modelo dela base de datos a crear las variables dentro
+    //despues de crear el modelo se crea el controlador del modelo
+    public class Account
+    {
+        public string Name { set; get; }
+        public string Password { set; get; }
+    }
+}
